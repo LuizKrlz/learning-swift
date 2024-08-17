d9ad39366f26168f61ddc57431f442595a7cc563
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2024-08-17 20:59:28 +0100

    Initial Commit

---
+++ card-view-swift-ui/card_view_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_view_swift_uiApp.swift
+//  card-view-swift-ui
+//
+//  Created by Luiz Correa on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct card_view_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
